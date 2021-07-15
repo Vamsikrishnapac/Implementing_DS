d8cefdc9fcf996bab5e4534f76ab3cdc874f7723
Vamsikrishnapac <86925499+Vamsikrishnapac@users.noreply.github.com>
2021-07-15 15:27:21 +0530

    Create Heap.cpp

---
+++ Heap.cpp
@@ -0,0 +1,8 @@
+// A heap is a special tree - based data structure in which the tree is a complete binary tree. 
+
+
+// Max - Heap: In a max-heap the key present at the root node must be greatest among the keys present at all of it's children. the same property must be recursively true for all sub-trees in that binary tree 
+
+// Min - Heap: In a Min - heap the key present at the root node must be minimum among the keys present at all of it's children. the same property must be recursively true for all sub - trees in that binary tree
+
+#include 
