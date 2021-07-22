4e1d3d5e2611ddcd1e6e1f53b71a2440b005956e
Vamsikrishnapac <86925499+Vamsikrishnapac@users.noreply.github.com>
2021-07-22 20:42:56 +0530

    Update and rename array to Starting_an_array.cpp

---
+++ Array/Starting_an_array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[3];
+    arr[0] =1;
+    arr[1] = 2;
+    arr[2]= 3;
+    cout<<arr[2]; // will print 3
+    
+    return 0;
+}
